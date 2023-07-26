214a275901d42fa2100bd6415b9dd04f0a81cdfb
Bhupendra Singh <bsingh1357@gmail.com>
2023-07-26 15:29:40 +0530

    Use Hash function

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 
@@ -17,8 +17,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password:String
 })
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User",userSchema);
 
@@ -38,7 +36,7 @@ app.post("/register",function(req,res){
     
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save()
@@ -51,7 +49,7 @@ app.post("/login",function(req,res){
     
     User.findOne({email: req.body.username})
     .then(function(found){
-        if(found.password === req.body.password){
+        if(found.password === md5(req.body.password)){
             res.render("secrets");
         }
     })
