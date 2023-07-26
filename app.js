2b297bf22a171b01068d9fdde2ad7b769c2f969d
Bhupendra Singh <bsingh1357@gmail.com>
2023-07-26 16:07:16 +0530

    Use salting and bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -33,25 +35,32 @@ app.get("/register",function(req,res){
 })
 
 app.post("/register",function(req,res){
-    
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
 
-    newUser.save()
-    .then(function(){
-        res.render("secrets");
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+        newUser.save()
+        .then(function(){
+            res.render("secrets");
+        })
+    });
+
+    
 })
 
 app.post("/login",function(req,res){
     
     User.findOne({email: req.body.username})
     .then(function(found){
-        if(found.password === md5(req.body.password)){
-            res.render("secrets");
-        }
+
+        bcrypt.compare(req.body.password, found.password, function(err, result) {
+            if(result===true){
+                res.render("secrets");
+            }
+        });
+        
     })
 })
 
